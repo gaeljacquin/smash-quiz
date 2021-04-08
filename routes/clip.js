2e0613c0f33b1b93e270aec9fc90f9cf2921bb47
gaeljacquin <8724132+gaeljacquin@users.noreply.github.com>
2021-04-07 20:34:29 -0400

    feat: add clip response

---
+++ routes/clip.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const fetch = require('node-fetch')
+const { Sequelize } = require('sequelize')
+
+const app = express()
+const sequelize = new Sequelize(process.env.DATABASE_URL)
+const query = `SELECT c.youtube_id, c.timer, string_agg(a.smash_id::text, ',') AS smash_ids
+  FROM clip c
+  LEFT JOIN answer a on c.id = a.clip_id
+  GROUP BY c.youtube_id, c.timer
+  ORDER BY random()
+  LIMIT 1
+`
+
+app.get('/', async (req, res) => {
+  await sequelize
+    .query(query, { type: sequelize.QueryTypes.SELECT })
+    .then(function (result) {
+      const clip = { clip: result }
+      res.json(clip)
+    })
+})
+
+module.exports = app
