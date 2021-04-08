2fac9862fb2dda0ddc8f614a179713adcc15d15b
gaeljacquin <8724132+gaeljacquin@users.noreply.github.com>
2021-04-07 20:34:17 -0400

    feat: add fighters response

---
+++ routes/fighters.js
@@ -0,0 +1,25 @@
+const createError = require('http-errors')
+const express = require('express')
+const fetch = require('node-fetch')
+const { Sequelize } = require('sequelize')
+require('dotenv').config()
+
+const app = express()
+const sequelize = new Sequelize(process.env.DATABASE_URL)
+const Fighters = sequelize.define(
+  'fighter',
+  {
+    smash_id: Sequelize.STRING,
+    name: Sequelize.STRING
+  },
+  {
+    freezeTableName: true,
+    timestamps: false
+  }
+)
+
+app.get('/', async (req, res) => {
+  Fighters.findAll().then((fighter) => res.json(fighter))
+})
+
+module.exports = app
