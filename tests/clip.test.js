2b5ab18324cf7dbc8f917587b8f726185f998b88
gaeljacquin <8724132+gaeljacquin@users.noreply.github.com>
2021-04-07 21:30:26 -0400

    test: add clip test

---
+++ tests/clip.test.js
@@ -0,0 +1,22 @@
+const expect = require('chai').expect
+const request = require('supertest')
+const _ = require('lodash')
+
+const app = require('../app')
+
+describe('GET /clip', () => {
+  it('OK, getting a random clip from the DB', (done) => {
+    request(app)
+      .get(`/clip`)
+      .then((res) => {
+        const body = res.body
+        expect(res.status).to.equal(200)
+        expect(res.type).to.equal('application/json')
+        expect(body.length).to.not.equal(0)
+        expect(body.clip.length).to.not.equal(0)
+        expect(body.clip.length).to.equal(1)
+        done()
+      })
+      .catch((err) => done(err))
+  })
+})
